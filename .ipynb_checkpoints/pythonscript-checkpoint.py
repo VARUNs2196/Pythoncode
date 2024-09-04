a7317f44a4298f5eca7e545286707eaac884b31e
VARUN SACHDEVA <VARUN.15987@stu.upes.ac.in>
2024-09-04 16:50:55 +0530

    Completing 3 questions

---
+++ .ipynb_checkpoints/pythonscript-checkpoint.py
@@ -0,0 +1,6 @@
+import sys
+name=input("\nEnter your name:")
+course_name=input("Enter your course name:")
+print("\nName: ",name)
+print("Course Name: ",course_name)
+print("Python version: ",sys.version,"\n")
